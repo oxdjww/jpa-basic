fdf984ba43576db79a5fee4a230523873a36ae8b
oxdjww <oxdjww@gmail.com>
2023-10-08 00:47:48 +0900

    feat : jpql test

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,11 +19,17 @@ public class JpaMain {
 
         // code
         try {
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+            // jpql
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5) // paging
+                    .setMaxResults(8)
+                    .getResultList();
 
-            em.persist(member);
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
