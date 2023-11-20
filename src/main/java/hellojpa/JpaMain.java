8af617a8f751815aa9bff9b64c9bb4f60174bb9a
oxdjww <oxdjww@gmail.com>
2023-11-20 18:28:56 +0900

    feat : 연관관계 설정

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -26,10 +27,19 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
+            em.flush();
+            em.clear();
+
             Member findMember = em.find(Member.class, member.getId());
 
             Team team1 = findMember.getTeam();
             System.out.println("team1.getName() = " + team1.getName());
+
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member member1 : members) {
+                System.out.println("member1.getUsername() = " + member1.getUsername());
+            }
 
             tx.commit();
         } catch (Exception e) {
